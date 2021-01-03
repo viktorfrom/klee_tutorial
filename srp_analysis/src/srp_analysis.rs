3f4b86e64fbf6aa7b0d2ca06781b37740829bd83
Viktor From <viktor.from91@gmail.com>
2021-01-03 16:37:38 +0100

    Remove blocking_time test

--- srp_analysis/src/srp_analysis.rs
+++ srp_analysis/src/srp_analysis.rs
@@ -179,20 +179,72 @@ mod parse_tests {
     use super::*;
 
     #[test]
-    fn test_blocking() {
-        let tasks = tasks();
-        let (ip, tr) = pre_analysis(&tasks);
-        let blocking_time1 = blocking_time(&tasks[0], &tasks, &ip, &tr);
-        assert_eq!(blocking_time1, 0.0);
-        let blocking_time2 = blocking_time(&tasks[1], &tasks, &ip, &tr);
-        assert_eq!(blocking_time2, 0.0);
-        let blocking_time3 = blocking_time(&tasks[2], &tasks, &ip, &tr);
-        assert_eq!(blocking_time3, 4.0);
-    }
-
-    #[test]
     fn test_preemption() {
-        let tasks = tasks();
+        let t1 = Task {
+            id: "T1".to_string(),
+            prio: 1,
+            deadline: 100,
+            inter_arrival: 100,
+            trace: Trace {
+                id: "T1".to_string(),
+                start: 0,
+                end: 10,
+                inner: vec![],
+            },
+        };
+
+        let t2 = Task {
+            id: "T2".to_string(),
+            prio: 2,
+            deadline: 200,
+            inter_arrival: 200,
+            trace: Trace {
+                id: "T2".to_string(),
+                start: 0,
+                end: 30,
+                inner: vec![
+                    Trace {
+                        id: "R1".to_string(),
+                        start: 10,
+                        end: 20,
+                        inner: vec![Trace {
+                            id: "R2".to_string(),
+                            start: 12,
+                            end: 16,
+                            inner: vec![],
+                        }],
+                    },
+                    Trace {
+                        id: "R1".to_string(),
+                        start: 22,
+                        end: 28,
+                        inner: vec![],
+                    },
+                ],
+            },
+        };
+
+        let t3 = Task {
+            id: "T3".to_string(),
+            prio: 3,
+            deadline: 50,
+            inter_arrival: 50,
+            trace: Trace {
+                id: "T3".to_string(),
+                start: 0,
+                end: 30,
+                inner: vec![Trace {
+                    id: "R2".to_string(),
+                    start: 10,
+                    end: 20,
+                    inner: vec![],
+                }],
+            },
+        };
+
+        // builds a vector of tasks t1, t2, t3
+        let tasks: Tasks = vec![t1, t2, t3];
+
         let (ip, tr) = pre_analysis(&tasks);
         let exact = srp_analysis(&tasks, &ip, &tr, false);
         assert_eq!(exact[0].4, 90.0);
